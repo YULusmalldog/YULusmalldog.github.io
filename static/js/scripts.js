a39441e258e44f160b478043c20b8742879527a5
YULusmalldog <161368064+YULusmalldog@users.noreply.github.com>
2024-11-22 15:15:18 +0100

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'talks','teaching']
+const section_names = ['home', 'publications', 'talks','teaching','miscellanous']
 
 
 window.addEventListener('DOMContentLoaded', event => {
